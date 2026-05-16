354af401b038374ac39bf173c56794fd09c67fa9
Lionel Desorme <lionel.desorme@free.fr>
2026-05-16 10:21:44 +0200

    Update

--- dir_timestamp.ps1
+++ dir_timestamp.ps1
@@ -1,1 +1,4 @@
-Get-ChildItem * | Sort-Object -Property LastWriteTime -Descending | Select-Object FullName, LastWritetime
+#Get-ChildItem * | Sort-Object -Property LastWriteTime -Descending | Select-Object FullName, LastWritetime
+
+Get-ChildItem * | Sort-Object -Property LastWriteTime -Descending | Select-Object @{n='LastWriteTime';e={'{0:yyyy-MM-dd HH:mm:ss}' -f $_.LastWriteTime}}, Name, Length
+
